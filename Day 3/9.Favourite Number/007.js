8c8f5ac7aae110572bd767ccdc094a24329d7ee4
AsmaIqbal01 <azeecreations1@gmail.com>
2024-04-02 14:00:36 +0500

    modifying

---
+++ Day 3/9.Favourite Number/007.js
@@ -0,0 +1,4 @@
+/*Favorite Number: Store your favorite number in a variable. Then, using that variable,
+create a message that reveals your favorite number. Print that message.*/
+var favNum = 2;
+console.log("My favourite number is: ".concat(favNum));
--- Favourite Number/007.js
+++
@@ -1,2 +0,0 @@
-var favNum = 2;
-console.log("My favourite number is: ".concat(favNum));
