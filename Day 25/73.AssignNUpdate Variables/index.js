3a7f10fd349c10678fe29082834aff1defe489c7
AsmaIqbal01 <azeecreations1@gmail.com>
2024-07-09 12:27:51 +0500

    Day 25

---
+++ Day 25/73.AssignNUpdate Variables/index.js
@@ -0,0 +1,9 @@
+/*Create a function where you declare a variable using let and assign an initial value. Then,
+update its value within the same function and log both the initial and updated values.*/
+function declareVariable() {
+    var username = "Smith";
+    console.log("Initail value of let username:", username);
+    username = "Diana";
+    console.log("Updated value of username:", username);
+}
+declareVariable();
