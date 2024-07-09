59f1b9dda3f137c3f4883c126e7d36d2114bc236
AsmaIqbal01 <azeecreations1@gmail.com>
2024-07-09 09:11:56 +0500

    Day 21

--- Day 21/63.Shape Shifter/index.ts
+++ Day 21/63.Shape Shifter/index.ts
@@ -1,2 +1,22 @@
 /*Shape Shifter: Write a program that can describe either a circle or a rectangle using a special type alias, including properties unique to each shape.*/
 //type alias to create a custom type for either a circle or rectangle
+type Shape={
+    name:"Circle" | "Reactangle";//name of the shape
+    radius?:number,//radius of the circle
+    width?:number,//width of the rectangle
+    height?:number//height of the rectangle
+};
+// describe if circle 
+let c:Shape={
+    name:"Circle",
+    radius:5
+};
+//describe if rectangle
+let r:Shape={
+    name:"Reactangle",
+    width:10,
+    height:5
+};
+//log both shapes
+console.log("Shape of a circle:",c);
+console.log("Shape of a rectangle",r);
