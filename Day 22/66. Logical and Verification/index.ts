e2510ba9e2d0a7ae83a8d2edfd0eb722f0882112
AsmaIqbal01 <azeecreations1@gmail.com>
2024-07-09 10:02:05 +0500

    Day 22

---
+++ Day 22/66. Logical and Verification/index.ts
@@ -0,0 +1,21 @@
+/*Logical AND Verification: Create a function that checks two boolean (true or false) values. 
+It should only say true if both are true, using the && operator. For instance, checkBothTrue(true, false) should be false.*/
+function checkBothTrue(value1: boolean, value2: boolean): boolean {
+    return value1 && value2;
+  }
+
+/*
+
+Explanation:
+The `checkBothTrue` function takes two boolean values as input, `value1` and `value2`. It uses the logical AND operator (`&&`) to check if both `value1` and `value2` are true. If both are true, the function returns `true`; otherwise, it returns `false`.
+
+Here are some examples of how to use the `checkBothTrue` function:
+*/
+console.log("If both are true",checkBothTrue(true, true));
+// Output: true
+
+console.log("If both are false",checkBothTrue(true, false));
+// Output: false
+
+console.log("If one is true",checkBothTrue(false, false));
+// Output: false
